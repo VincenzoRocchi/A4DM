c90732664d44459d3b0ae31ac2753e440cb375f6
vincenzo_rocchi <vincenzo.rocchi@gmail.com>
2023-11-21 17:13:38 +0100

    countig sort and new small array function

--- functions.py
+++ functions.py
@@ -4,6 +4,10 @@ def new_array():
     A = [random.randint(1, 100) for _ in range(10)]
     return A
 
+def new_array_s():
+    A = [random.randint(1, 100) for _ in range(5)]
+    return A
+
 def swap_ind(A,a,b):
     A[a], A[b] = A[b], A[a]
     
